2eb57d23300397773b5cb0082a81ac96ac1d8620
sudarshan bhat <sudarshanrbhat.srb2@gmail.com>
2023-11-08 19:00:55 +0530

    second commit

--- backend.js
+++ backend.js
@@ -1,19 +1,53 @@
 const express = require('express')
+const mysql = require('mysql');
 const path = require('path')
 const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+const db = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'bill',
+  });
+  
+  db.connect((err)=>{
+    if(err){
+      console.log('Database connection error:', err);
+      return;
+    }
+    console.log('Connected to the database')
+  });
+
+let Username; 
+let Password; 
+  
 app.post('/submit', (req, res) => {
-    const username= req.body.username
-    const password = req.body.password;
-    res.send(`You entered: ${username} as a username and ${password} as password`);
+    Username = req.body.username; 
+    Password = req.body.password;
+    // res.send(`You entered: ${username} as a username and ${password} as password`);
+    
+    const data = {
+        username: Username,
+        password: Password
+    }
+    const sql = 'INSERT INTO details SET ?';
+    db.query(sql, data, (err, result) => {
+        if (err) {
+            console.error('SQL query error:', err);
+            return;
+        }
+        console.log('Data added to the database');
+        res.send('Data added to the database');
+    });
 });
 
 app.listen(port, () => {
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mysql = require('mysql');
 const path = require('path')
 const app = express()
 const port = 3000
@@ -10,6 +11,25 @@ app.get('/', (req, res) => {
 app.get('/about',(req,res)=>{
   res.sendFile(path.join(__dirname,'index.html'))
 })
+
+const db = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'root',
+  database:'bill',
+});
+
+db.connect((err)=>{
+  if(err){
+    console.log('Database connection error:', err);
+    return;
+  }
+  console.log('Connected to the database')
+})
+
+app.post('/addData',(req,res)=>{
+  
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
